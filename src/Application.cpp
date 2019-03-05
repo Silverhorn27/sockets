14625bb80065fe44d6bbe64e1dd87560935babb2
Silverhorn27 <grenwood27sg@gmail.com>
2019-03-05 20:51:17 +0200

    Added argument checking for _server.init()

--- src/Application.cpp
+++ src/Application.cpp
@@ -4,7 +4,11 @@ Application::Application(int argc, char **argv)
     : _logger(StringUtils::toString("Application ", StringUtils::hexToString(this)))
 {
     init(argc, argv);
-    _server.init(argv[1], atoi(argv[2]));
+    if (_args.size() == 3) {
+        _server.init(argv[1], atoi(argv[2]));
+    } else {
+        _logger.log(Logger::Error, "Was passed wrong arguments");
+    }
 }
 
 void Application::init(int argc, char **argv)
