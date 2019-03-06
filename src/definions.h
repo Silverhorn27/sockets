9633fb77ce5104adc2bd9fd08acc2fb3ff72929a
Silverhorn27 <grenwood27sg@gmail.com>
2019-03-06 22:30:44 +0200

    added vector

--- src/definions.h
+++ src/definions.h
@@ -2,7 +2,13 @@
 #define DEFINIONS_H
 
 #include <string>
+#include <vector>
 
 using std::string;
+using std::vector;
+
+using Buffer = vector<char>;
+
 #define DEFAULT_BUFFER_SIZE 1024
+
 #endif // DEFINIONS_H
